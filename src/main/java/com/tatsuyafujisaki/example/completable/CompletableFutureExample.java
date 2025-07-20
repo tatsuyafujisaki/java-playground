863ea3fa9e414de483ff7b894d37c530bd7d577f
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2025-07-20 11:35:14 +0900

    Delete CallableToCompletableFutureExample.java

--- src/main/java/com/tatsuyafujisaki/example/completable/CompletableFutureExample.java
+++ src/main/java/com/tatsuyafujisaki/example/completable/CompletableFutureExample.java
@@ -4,43 +4,49 @@ import java.time.Duration;
 import java.util.Random;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+import java.util.function.Consumer;
 
 /// [CompletableFuture](https://docs.oracle.com/en/java/javase/24/docs/api/java.base/java/util/concurrent/CompletableFuture.html)
 public class CompletableFutureExample {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         example1();
-        System.out.println("--");
         example2();
     }
 
     private static void example1() {
-        try {
-            System.out.println(createCompletableFuture().join());
-        } catch (Exception e) {
-            System.err.println(e.getMessage());
-        }
-    }
-
-    private static void example2() {
-        createCompletableFuture()
-                .handle((data, throwable) -> {
+        mockThirdPartyApi()
+                .whenComplete((data, throwable) -> {
                             if (throwable == null) {
                                 System.out.println(data);
                             } else {
                                 System.err.println(throwable.getMessage());
                             }
-                            return null;
                         }
-                ).join();
+                );
     }
 
-    private static CompletableFuture<String> createCompletableFuture() {
-        return CompletableFuture.supplyAsync(() -> {
-                    sleep(Duration.ofMillis(100));
-                    mayThrowException();
-                    return "🍎";
-                }
-        );
+    private static void example2() {
+        try {
+            System.out.println(mockThirdPartyApi().join());
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    private static CompletableFuture<String> mockThirdPartyApi() {
+        var future = new CompletableFuture<String>();
+        mockThirdPartyCallbackApi(future::complete, future::completeExceptionally);
+        return future;
+    }
+
+    /// Mock third-party API that does not return a value, but rather takes callbacks and provides a success or failure value via one of them.
+    private static void mockThirdPartyCallbackApi(Consumer<String> onSuccess, Consumer<Exception> onError) {
+        try {
+            mayThrowException();
+            onSuccess.accept("🍎");
+        } catch (Exception e) {
+            onError.accept(e);
+        }
     }
 
     private static void sleep(Duration duration) {
